a6b1ceb2b89c79ecf4a34333573a9ae2978c591e
DESKTOP-69UC1S4\박한솔 <sol25890@gmail.com>
2025-01-21 11:15:40 +0900

    [Gordon] 로그인시 유저조회 await 추가

--- src/auth/member.service.ts
+++ src/auth/member.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from "bcryptjs";
 import { JwtService } from '@nestjs/jwt';
@@ -32,11 +32,11 @@ export class MemberService {
         memberSignInDto: MemberSignInDto
     ): Promise<ResponseDto> {
         const { userId, password } = memberSignInDto;
-        const user = this.memberRepository.findOneBy({ userId });
-        
+        const user = await this.memberRepository.findOneBy({ userId });
+
         if(user){
             // 입력 아이디와 같은 유저가 있다면
-            if((await bcrypt.compare(password, (await user).password))){
+            if((await bcrypt.compare(password, user.password))){
                 // 비밀번호도 맞는지 확인
                 
                 // 유저 토큰 생성
@@ -44,11 +44,11 @@ export class MemberService {
                 const refreshToken = await this.generateRefreshToken(userId);
 
                 // Redis에 토큰 저장
-                await this.redisService.set(
-                    accessToken, //key
-                    userId, //value
-                    this.configService.get("REDIS_CACHE_TTL") //ttl
-                );
+                // await this.redisService.set(
+                //     accessToken, //key
+                //     userId, //value
+                //     this.configService.get("REDIS_CACHE_TTL") //ttl
+                // );
                 await this.redisService.set(
                     refreshToken, //key
                     userId, //value
@@ -62,11 +62,11 @@ export class MemberService {
                 this.responseDto.data = {accessToken, refreshToken};
             } else {
                 // 비밀번호가 틀린경우
-                throw new UnauthorizedException(`로그인 실패, 비밀번호를 확인해주세요.`);
+                throw new BadRequestException(`로그인 실패, 비밀번호를 확인해주세요.`);
             }
         } else {
             // 입력 아이디와 같은 유저가 없다면
-            throw new UnauthorizedException(`로그인 실패, 아이디를 확인해주세요.`);
+            throw new BadRequestException(`로그인 실패, 아이디를 확인해주세요.`);
         }
 
         return this.responseDto;
@@ -101,17 +101,6 @@ export class MemberService {
         )
 
         return token
-    }
-
-    // token 검증
-    private async validateUser(userId): Promise<boolean> {
-        const user = this.memberRepository.findOneBy({ userId });
-
-        if(!user) {
-            throw new UnauthorizedException("can't find user...");
-        } else {
-            return true;
-        }
     }
     // Token (E)
 }
